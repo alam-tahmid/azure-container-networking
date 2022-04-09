899960a2eefcd180d25fce2c9851bd7cde6374a5
alegal <aegal@outlook.com>
2022-04-09 08:31:29 -0700

    address comments

--- network/network_windows_test.go
+++ network/network_windows_test.go
@@ -11,6 +11,8 @@ import (
 	"testing"
 
 	"github.com/Azure/azure-container-networking/network/hnswrapper"
+
+	"github.com/Microsoft/hcsshim/hcn"
 )
 
 func TestNewAndDeleteNetworkImplHnsV2(t *testing.T) {
@@ -41,6 +43,41 @@ func TestNewAndDeleteNetworkImplHnsV2(t *testing.T) {
 	}
 
 	err = nm.deleteNetworkImplHnsV2(network)
+
+	if err != nil {
+		fmt.Printf("+%v", err)
+		t.Fatal(err)
+	}
+}
+
+func TestSuccesfulNetworkCreationWhenAlreadyExists(t *testing.T) {
+	nm := &networkManager{
+		ExternalInterfaces: map[string]*externalInterface{},
+	}
+
+	// this hnsv2 variable overwrites the package level variable in network
+	// we do this to avoid passing around os specific objects in platform agnostic code
+	hnsv2 = hnswrapper.NewHnsv2wrapperFake()
+
+	network := &hcn.HostComputeNetwork{
+		Name: "azure-vlan1-172-28-1-0_24",
+	}
+
+	_, err := hnsv2.CreateNetwork(network)
+
+	// network name is derived from network info id
+	nwInfo := &NetworkInfo{
+		Id:           "azure-vlan1-172-28-1-0_24",
+		MasterIfName: "eth0",
+		Mode:         "bridge",
+	}
+
+	extInterface := &externalInterface{
+		Name:    "eth0",
+		Subnets: []string{"subnet1", "subnet2"},
+	}
+
+	_, err = nm.newNetworkImplHnsV2(nwInfo, extInterface)
 
 	if err != nil {
 		fmt.Printf("+%v", err)
